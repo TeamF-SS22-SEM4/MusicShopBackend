5384fd92c32f71f8790c4f8d7a3b8f249a8b669f
Smightym8 <michaelspiegel@live.at>
2022-03-21 13:06:48 +0100

    Implemented RMI Server
    Co-authored-by: Smilyfail <dbi6423@students.fhv.at>

---
+++ src/main/java/at/fhv/ss22/ea/f/musicshop/backend/application/impl/RMIFactoryImpl.java
@@ -0,0 +1,18 @@
+package at.fhv.ss22.ea.f.musicshop.backend.application.impl;
+
+import at.fhv.ss22.ea.f.communication.api.ProductSearchService;
+import at.fhv.ss22.ea.f.communication.api.RMIFactory;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMIFactoryImpl extends UnicastRemoteObject implements RMIFactory {
+    public RMIFactoryImpl() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public ProductSearchService getProductSearchService() throws RemoteException {
+        return ProductSearchServiceImpl.newInstance();
+    }
+}
---
+++ src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/RMIServer.java
@@ -0,0 +1,29 @@
+package at.fhv.ss22.ea.f.musicshop.backend.communication;
+
+import at.fhv.ss22.ea.f.communication.api.RMIFactory;
+import at.fhv.ss22.ea.f.musicshop.backend.application.impl.RMIFactoryImpl;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RMIServer {
+    private static String PROTOCOL = "rmi://";
+    private static String HOST = "localhost";
+    private static String STUB = "/RMIFactory";
+
+    public static void startRMIServer() {
+        try {
+            LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+
+            RMIFactory rmiFactory = new RMIFactoryImpl();
+
+            Naming.rebind(PROTOCOL + HOST + STUB, rmiFactory);
+            System.out.println("RMIFactory bound in registry");
+        } catch (RemoteException | MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
