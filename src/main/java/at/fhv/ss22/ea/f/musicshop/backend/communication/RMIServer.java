331658fa1ce8e1e7cfc3fe73ab213e42b4347a92
Smightym8 <michaelspiegel@live.at>
2022-03-22 20:38:34 +0100

    Changes according to sharedLib

--- src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/RMIServer.java
+++ src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/RMIServer.java
@@ -10,7 +10,7 @@ import java.rmi.registry.LocateRegistry;
 public class RMIServer {
     private static int PORT = 12345;
     private static String PROTOCOL = "rmi://";
-    private static String HOST = "127.0.0.1";
+    private static String HOST = "10.0.40.170";
     private static String STUB = "/RMIFactory";
 
     public static void startRMIServer() {
