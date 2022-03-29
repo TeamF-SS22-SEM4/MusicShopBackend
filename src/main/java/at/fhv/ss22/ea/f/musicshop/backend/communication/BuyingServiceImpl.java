44fe91a4122d2426e5167e41c143b525fa521460
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-03-29 17:59:06 +0200

    buy with cartDTO

--- src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/BuyingServiceImpl.java
+++ src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/BuyingServiceImpl.java
@@ -1,6 +1,7 @@
 package at.fhv.ss22.ea.f.musicshop.backend.communication;
 
 import at.fhv.ss22.ea.f.communication.api.BuyingService;
+import at.fhv.ss22.ea.f.communication.dto.ShoppingCartProductDTO;
 import at.fhv.ss22.ea.f.communication.dto.SoundCarrierAmountDTO;
 import at.fhv.ss22.ea.f.communication.exception.CarrierNotAvailableException;
 import at.fhv.ss22.ea.f.musicshop.backend.application.api.BuyingApplicationService;
@@ -9,6 +10,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class BuyingServiceImpl extends UnicastRemoteObject implements BuyingService {
 
@@ -22,5 +24,17 @@ public class BuyingServiceImpl extends UnicastRemoteObject implements BuyingServ
     @Override
     public UUID buy(List<SoundCarrierAmountDTO> soundCarriers, String paymentMethod) throws CarrierNotAvailableException {
         return buyingApplicationService.buy(soundCarriers, paymentMethod);
+    }
+
+    @Override
+    public UUID buyWithShoppingCart(List<ShoppingCartProductDTO> cartDtos, String paymentMethod) throws CarrierNotAvailableException {
+        List<SoundCarrierAmountDTO> carrierAmountDTOS = cartDtos.stream().map(cartDTO ->
+            SoundCarrierAmountDTO.builder()
+                    .withAmount(cartDTO.getSelectedAmount())
+                    .withCarrierId(cartDTO.getSoundCarrierId())
+                    .build()
+        ).collect(Collectors.toList());
+
+        return buyingApplicationService.buy(carrierAmountDTOS, paymentMethod);
     }
 }
