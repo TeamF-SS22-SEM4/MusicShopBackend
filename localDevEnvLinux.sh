8cf43150b3e33bb762d3324dcff7309d30341e55
Smightym8 <michaelspiegel@live.at>
2022-04-20 07:59:19 +0200

    Fixed localDevEnvLinux Script to use newest build

--- localDevEnvLinux.sh
+++ localDevEnvLinux.sh
@@ -1,3 +1,4 @@
-sudo docker-compose down
+docker-compose down
 ./gradlew build
-sudo docker-compose -f docker-compose.yml -f docker-compose.local.yml up -d
+docker-compose build
+docker-compose -f docker-compose.yml -f docker-compose.local.yml up -d
