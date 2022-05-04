fd664b700fdfb9031daae7172caa68a6a036f229
Smightym8 <michaelspiegel@live.at>
2022-05-04 13:01:04 +0200

    Added customer users

--- src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/rest/ProductController.java
+++ src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/rest/ProductController.java
@@ -28,11 +28,11 @@ public class ProductController {
             ProductDetailsDTO product = productApplicationService.productById(sessionId, UUID.fromString(productId));
             return Response.ok().entity(product).build();
         } catch (SessionExpired e) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         } catch (NoPermissionForOperation e) {
-            return Response.status(403).build();
+            return Response.status(Response.Status.FORBIDDEN).build();
         } catch (NoSuchElementException e) {
-            return Response.status(400).entity("Product with id " + productId + " not found").build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("Product with id " + productId + " not found").build();
         }
     }
 
@@ -44,9 +44,9 @@ public class ProductController {
             List<ProductOverviewDTO> products = productApplicationService.search(sessionId, query);
             return Response.ok().entity(products).build();
         } catch (SessionExpired e) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         } catch (NoPermissionForOperation e) {
-            return Response.status(403).build();
+            return Response.status(Response.Status.FORBIDDEN).build();
         }
     }
 }
