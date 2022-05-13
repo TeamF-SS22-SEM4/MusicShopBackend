5189f1ceca56675ff5bcfcb39e3951c807190cc7
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-05-13 17:18:17 +0200

    added rolecheck test

---
+++ src/test/java/at/fhv/ss22/ea/f/musicshop/backend/unit/application/RoleCheckTests.java
@@ -0,0 +1,62 @@
+package at.fhv.ss22.ea.f.musicshop.backend.unit.application;
+
+import at.fhv.ss22.ea.f.communication.dto.SoundCarrierAmountDTO;
+import at.fhv.ss22.ea.f.communication.exception.CarrierNotAvailableException;
+import at.fhv.ss22.ea.f.communication.exception.NoPermissionForOperation;
+import at.fhv.ss22.ea.f.communication.exception.SessionExpired;
+import at.fhv.ss22.ea.f.musicshop.backend.application.api.AuthenticationApplicationService;
+import at.fhv.ss22.ea.f.musicshop.backend.application.api.SaleApplicationService;
+import at.fhv.ss22.ea.f.musicshop.backend.application.impl.AuthenticationApplicationServiceImpl;
+import at.fhv.ss22.ea.f.musicshop.backend.application.impl.SaleApplicationServiceImpl;
+import at.fhv.ss22.ea.f.musicshop.backend.application.impl.decorators.RoleCheckInterceptor;
+import at.fhv.ss22.ea.f.musicshop.backend.domain.model.UserRole;
+import at.fhv.ss22.ea.f.musicshop.backend.domain.model.session.SessionId;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import javax.interceptor.InvocationContext;
+import javax.persistence.criteria.CriteriaBuilder;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class RoleCheckTests {
+    private RoleCheckInterceptor roleCheckInterceptor;
+    private AuthenticationApplicationService authService;
+
+    @BeforeAll
+    void setup() {
+        authService = mock(AuthenticationApplicationService.class);
+        roleCheckInterceptor = new RoleCheckInterceptor(authService);
+    }
+
+    @Test
+    void call_method_with_valid_permissions() throws Exception {
+        //given
+        InvocationContext context = mock(InvocationContext.class);
+        when(authService.hasRole(any(), any())).thenReturn(true);
+        Method method = TestingInterfaceValid.class.getMethod("testing", String.class, Integer.class);
+        when(context.getParameters()).thenReturn(new Object[]{"some-session-id,", 2});
+
+        when(context.getMethod()).thenReturn(method);
+        when(context.proceed()).thenReturn(true);
+
+        //whhen
+        roleCheckInterceptor.intercept(context);
+
+        //then
+        verify(context, times(1)).proceed();
+        verify(authService, times(1)).hasRole(any(), any());
+    }
+
+}
