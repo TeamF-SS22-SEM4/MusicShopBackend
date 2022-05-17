39c6c443c02ed5f9ead2fda71fe5517a3710625d
Smightym8 <michaelspiegel@live.at>
2022-05-17 18:12:51 +0200

    Worked on connection refused error

--- src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/rest/client/RestClientImpl.java
+++ src/main/java/at/fhv/ss22/ea/f/musicshop/backend/communication/rest/client/RestClientImpl.java
@@ -1,26 +1,26 @@
 package at.fhv.ss22.ea.f.musicshop.backend.communication.rest.client;
 
-import io.netty.handler.codec.http.HttpMethod;
-import reactor.netty.http.client.HttpClient;
-
 import javax.ejb.Local;
 import javax.ejb.Stateless;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
 
 @Stateless
 @Local(RestClient.class)
 public class RestClientImpl implements RestClient {
-    private final HttpClient httpClient = HttpClient.create();
+    Client client = ClientBuilder.newClient();
 
     @Override
     public void sendPurchaseToPlaylistMicroservice() {
         // TODO: Implement
-        // TODO: Fix error: Connection refused: playlist-microservice/172.22.0.5:8081
         // Dummy implementation
-        httpClient.baseUrl("http://playlist-microservice:8081")
-                .request(HttpMethod.POST)
-                .uri("/purchase")
-                .responseContent()
-                .asString()
-                .subscribe(System.out::println);
+        WebTarget target = client.target("http://playlist-microservice:8081").path("purchase");
+        target.request(MediaType.TEXT_PLAIN).post(
+                Entity.entity("hello", MediaType.TEXT_PLAIN), String.class
+        );
     }
 }
